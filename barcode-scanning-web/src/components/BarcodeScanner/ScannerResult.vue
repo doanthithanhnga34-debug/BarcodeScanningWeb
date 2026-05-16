648194027ea6c7ea23a2efcc13f192ce60a4bcfb
ThanhNgaaaa <doanthithanhnga3579@gmail.com>
2026-05-16 20:01:51 +0700

    add link image

--- barcode-scanning-web/src/components/BarcodeScanner/ScannerResult.vue
+++ barcode-scanning-web/src/components/BarcodeScanner/ScannerResult.vue
@@ -1,35 +1,45 @@
 <template>
-
+  <div class="px-3 py-6 z-50 fadeInUp">
     <div
-      class="  px-3 py-6  z-50 fadeInUp"
+      class="flex flex-row justify-around items-center p-4 bg-[#f3f3fd] rounded-lg mb-3 border-fushia-50 shadow-xs"
     >
-      <div class="flex justify-around items-center gap-3 p-4 bg-[#f3f3fd] rounded-lg mb-3 border-fushia-50 shadow-xs">
-        <div
-          class="flex icon-box items-center justify-centerw-20 h-20 rounded-xl bg-primary"
-        >
-          <i class="pi pi-image"></i>
-        </div>
-        <p v-if="loading" class="mt-1 text-sm font-bold text-gray-500">
-          Đang lấy thông tin...
-        </p>
-        <p v-else-if="product" class="text-sm text-gray-500 font-bold">
-          {{ productName }}
-        </p>
-        <p v-else class="text-sm text-red-500 font-bold">
-          {{ errorMessage || "Không tìm thấy sản phẩm" }}
-        </p>
-       
+      <div
+        class="flex basis-1/3 icon-box items-center justify-centerw-20 h-20 rounded-xl bg-primary"
+      >
+        <img
+          width="48"
+          height="48"
+          :src="
+            product.image
+              ? product.image
+              : 'https://img.icons8.com/forma-thin/48/product.png'
+          "
+          alt="product"
+        />
       </div>
-       <button
-          class="button flex items-center justify-center w-full bg-blue-700 p-4 rounded-xl  text-sm font-semibold text-white shadow-lg active:scale-[0.98] transition"
-          @click="isShowModal"
-        >
-         Xác nhận
-        </button>
-        <span class="mt-3 text-center text-xs text-slate-400">
-        Chạm “Xác nhận” để thiết lập hạn sử dụng
-      </span>
+      <p v-if="loading" class="mt-1 text-sm font-bold text-gray-500">
+        Đang lấy thông tin...
+      </p>
+      <p
+        v-else-if="product"
+        class="basis-2/3 text-sm text-gray-500 font-bold "
+      >
+        {{ productName }}
+      </p>
+      <p v-else class="text-sm text-red-500 font-bold">
+        {{ errorMessage || "Không tìm thấy sản phẩm" }}
+      </p>
     </div>
+    <button
+      class="button flex items-center justify-center w-full bg-blue-700 p-4 rounded-xl text-sm font-semibold text-white shadow-lg active:scale-[0.98] transition"
+      @click="isShowModal"
+    >
+      Xác nhận
+    </button>
+    <span class="mt-3 text-center text-xs text-slate-400">
+      Chạm “Xác nhận” để thiết lập hạn sử dụng
+    </span>
+  </div>
 
   <Transition name="modal">
     <ExpiryForm
